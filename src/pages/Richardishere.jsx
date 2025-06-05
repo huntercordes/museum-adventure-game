a39da8eedfcaf88109005f3c387db669f269501a
Richie <eaa24rhol@students.eaaa.dk>
2025-06-05 11:30:11 +0200

    prd
    prdko

---
+++ src/pages/Richardishere.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Richardishere = () => {
+    return (
+        <div>
+            <h1>Richard is here!</h1>
+            <p>Welcome to the Richardishere page.</p>
+        </div>
+    );
+};
+
+export default Richardishere;
